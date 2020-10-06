68342a536488cdd11c02a1cb70e35710e640523e
Steven Topazio <sat@northpointprinting.com>
2020-10-06 19:51:46 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -6,6 +6,7 @@ class Song
   
  def initialize(name)
    @name = name 
+   save
  end
  
  def self.new_by_filename(file)
@@ -22,5 +23,11 @@ class Song
    artist.add_song(self)
  end
  
- 
+ def save
+    @@all << self 
+  end
+  
+  def self.all
+    @@all
+  end
 end
